37b816301dd2e7086bef405aee9ce23814233d5b
ilcreminotozzo <208239669+ilcreminotozzo@users.noreply.github.com>
2026-04-17 16:20:01 +0200

    add supabase auth

---
+++ app/login/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) alert(error.message);
+    else alert("Login OK");
+  }
+
+  async function register() {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) alert(error.message);
+    else alert("Account creato!");
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Login A-Sharing</h1>
+
+      <input
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={login}>Login</button>
+      <button onClick={register}>Registrati</button>
+    </div>
+  );
+}
---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://gyvnbxhakzhkzdgeyhbu.supabase.co",
+  "sb_publishable_SDzu5nXiBqUnP4pXHjSjQA_ughKBB2F"
+);
