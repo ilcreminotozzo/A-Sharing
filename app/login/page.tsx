b37afc8e916d662f2583135bdfa03df74c434bda
ilcreminotozzo <208239669+ilcreminotozzo@users.noreply.github.com>
2026-04-17 17:55:23 +0200

    fix the login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,13 +3,8 @@
 export default function Login() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>TEST LOGIN PAGE</h1>
-
-      <button
-        onClick={() => alert("CLICK FUNZIONA")}
-        style={{ padding: 20, background: "red", color: "white" }}
-      >
-        TEST BUTTON
+      <button onClick={() => alert("JS FUNZIONA")}>
+        TEST
       </button>
     </div>
   );
